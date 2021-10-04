1181f41cc8b4cbb23fb886b0233fd5eb3d467eaa
mihaillatyshov <mihail.latyshov2@mail.ru>
2021-10-04 17:22:33 +0300

    fix premake

--- premake5.lua
+++ premake5.lua
@@ -1,61 +1,61 @@
 project "glew"
 	kind "StaticLib"
 	language "C"
-    staticruntime "on"
+	staticruntime "on"
 
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-    files
-    {
-        "include/GL/eglew.h",
-        "include/GL/glew.h",
-        "include/GL/glxew.h",
-        "include/GL/wglew.h",
+	files
+	{
+		"include/GL/eglew.h",
+		"include/GL/glew.h",
+		"include/GL/glxew.h",
+		"include/GL/wglew.h",
 
-        "src/glew.c"
-    }
+		"src/glew.c"
+	}
 
-    defines
-    {
-        "GLEW_STATIC",
-        "_LIB",
-        "GLEW_STATIC"
-    }
+	defines
+	{
+		"GLEW_STATIC",
+		"_LIB",
+		"GLEW_STATIC"
+	}
 
-    includedirs
-    {
-        "include"
-    }
+	includedirs
+	{
+		"include"
+	}
 
-    filter "system:windows"
+	filter "system:windows"
 		systemversion "latest"
 
-        defines
-        {
-            "WIN32",
-            "WIN32_LEAN_AND_MEAN"
-        }
-        
-        links
-        {
-            "opengl32"
-        }
-
-    filter "configurations:Debug"
+		defines
+		{
+			"WIN32",
+			"WIN32_LEAN_AND_MEAN"
+		}
+		
+		links
+		{
+			"opengl32"
+		}
+		
+	filter "configurations:Debug"
 		runtime "Debug"
 		symbols "on"
 
-        defines
-        {
-            "_DEBUG"
-        }
+		defines
+		{
+			"_DEBUG"
+		}
 
 	filter "configurations:Release"
 		runtime "Release"
 		optimize "on"
-        
-        defines
-        {
-            "NDEBUG"
-        }
+	    
+		defines
+		{
+			"NDEBUG"
+		}
